536be495712f120eefa551af32f278f74484a48f
noambialik <37776609+noambialik@users.noreply.github.com>
2021-02-25 15:14:56 +0200

    Create send_recv_main.ino

---
+++ send_recv_main/send_recv_main.ino
@@ -0,0 +1,48 @@
+#include <SoftwareSerial.h>
+#define setPin 6
+
+SoftwareSerial mySerial(10,11); //RX, TX
+String NAME = "Nati";
+
+void setup() {
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  
+  pinMode(6, OUTPUT);
+  digitalWrite(setPin, LOW);
+  delay(100);
+  
+  mySerial.write("AT+A001");
+  delay(100);
+  mySerial.write("AT+C001");
+  delay(100);
+  mySerial.write("AT+B9600");
+  delay(100);
+  mySerial.write("AT+FU3");
+  delay(100);
+  
+  digitalWrite(setPin, HIGH);
+}
+
+void loop() {
+  
+  
+  if(Serial.available() > 0){//Read from serial monitor and send over HC-12
+    String input = Serial.readString();
+    mySerial.println(NAME+": "+input); 
+    Serial.println("Me: "+input); 
+    //Serial.println(input);  
+    //mySerial.println(input); 
+
+   
+  }
+ 
+  if(mySerial.available() > 1){//Read from HC-12 and send to serial monitor
+    String input = mySerial.readString();
+    Serial.println(input);
+    //Serial.println(input);  
+       
+
+  }
+  
+}
